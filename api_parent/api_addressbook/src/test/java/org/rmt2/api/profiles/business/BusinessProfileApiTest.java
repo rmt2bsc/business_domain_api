67457a6c6b352ab1abc02949adde71051324bd88
roy terrell <rmt2bsc@gmail.com>
2017-04-21 11:51:42 -0500

    Removed from project

--- api_parent/api_addressbook/src/test/java/org/rmt2/api/profiles/business/BusinessProfileApiTest.java
+++ api_parent/api_addressbook/src/test/java/org/rmt2/api/profiles/business/BusinessProfileApiTest.java
@@ -6,50 +6,106 @@ package org.rmt2.api.profiles.business;
 import static org.mockito.Matchers.any;
 import static org.mockito.Mockito.when;
 
-import org.dao.mapping.orm.rmt2.Address;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.dao.contacts.ContactDaoException;
 import org.dao.mapping.orm.rmt2.Business;
+import org.dao.mapping.orm.rmt2.VwBusinessAddress;
 import org.dto.BusinessContactDto;
 import org.dto.ContactDto;
-import org.dto.adapter.jaxb.JaxbAddressBookFactory;
 import org.dto.adapter.orm.Rmt2AddressBookDtoFactory;
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.modules.contacts.ContactsApi;
 import org.modules.contacts.ContactsApiException;
+import org.modules.contacts.ContactsApiFactory;
 import org.powermock.core.classloader.annotations.PrepareForTest;
 import org.powermock.modules.junit4.PowerMockRunner;
-import org.rmt2.api.profiles.BaseProfileTest;
+import org.rmt2.api.BaseDaoTest;
+
+import com.api.persistence.AbstractDaoClientImpl;
+import com.api.persistence.db.orm.Rmt2OrmClientFactory;
 
 /**
  * @author royterrell
  *
  */
 @RunWith(PowerMockRunner.class)
-@PrepareForTest({ BaseProfileTest.class })
-public class BusinessProfileApiTest extends BaseProfileTest {
+@PrepareForTest({ AbstractDaoClientImpl.class, Rmt2OrmClientFactory.class })
+public class BusinessProfileApiTest extends BaseDaoTest {
+
+    private List<VwBusinessAddress> mockSingleProfileFetchResponse;
+
+    @Override
+    public void setUp() throws Exception {
+        APP_NAME = "addressbook";
+        super.setUp();
+        this.mockSingleProfileFetchResponse = this.createSingleContactFetchResponse();
+    }
+
+    @Override
+    public void tearDown() throws Exception {
+        super.tearDown();
+    }
+
+    private List<VwBusinessAddress> createSingleContactFetchResponse() {
+        List<VwBusinessAddress> list = new ArrayList<VwBusinessAddress>();
+        VwBusinessAddress b = new VwBusinessAddress();
+        b.setBusinessId(1351);
+        b.setBusLongname("Ticket Master");
+        b.setBusContactFirstname("roy");
+        b.setBusContactLastname("terrell");
+        b.setBusContactPhone("9728882222");
+        b.setBusServTypeId(130);
+        b.setBusEntityTypeId(100);
+        b.setBusTaxId("75-9847382");
+        b.setBusWebsite("ticketmaster.com");
+
+        b.setAddrId(2222);
+        b.setAddrBusinessId(1351);
+        b.setAddr1("94393 Hall Ave.");
+        b.setAddr2("Suite 948");
+        b.setAddr3("P.O. Box 84763");
+        b.setZipCity("dallas");
+        b.setZipState("TX");
+        b.setAddrZip(75028);
+        b.setAddrZipext(1234);
+
+        list.add(b);
+        return list;
+    }
 
     @Test
     public void testSingleBusinessContactFetch() {
         Business bus = new Business();
-        Address addr = new Address();
+        // Address addr = new Address();
 
         bus.setBusinessId(1351);
-        bus.setLongname("Ticket Master");
+        // bus.setLongname("Ticket Master");
 
-        addr.setAddrId(2258);
-        addr.setBusinessId(1351);
-        addr.setPhoneMain("2143738000");
+        // addr.setAddrId(2258);
+        // addr.setBusinessId(1351);
+        // addr.setPhoneMain("2143738000");
 
-        BusinessContactDto busDto = Rmt2AddressBookDtoFactory.getBusinessInstance(bus, addr);
-
-        BusinessContactDto dto = null;
-        dto = JaxbAddressBookFactory.createBusinessContactDtoInstance(contact);
+        BusinessContactDto busDto = Rmt2AddressBookDtoFactory.getBusinessInstance(bus, null);
         try {
-            when(this.mockDao.getContact(any(ContactDto.class))).thenReturn(this.mockSinglePRofileResponse);
-        } catch (ContactsApiException e) {
+            when(this.mockPersistenceClient.retrieveList(any(VwBusinessAddress.class)))
+                    .thenReturn(this.mockSingleProfileFetchResponse);
+        } catch (ContactDaoException e) {
 
         }
 
-        this.startTest();
+        ContactsApiFactory f = new ContactsApiFactory();
+        ContactsApi api = f.createApi(APP_NAME);
+        List<ContactDto> results = null;
+        try {
+            results = api.getContact(busDto);
+        } catch (ContactsApiException e) {
+            e.printStackTrace();
+        }
+        Assert.assertNotNull(results);
     }
 
 }
