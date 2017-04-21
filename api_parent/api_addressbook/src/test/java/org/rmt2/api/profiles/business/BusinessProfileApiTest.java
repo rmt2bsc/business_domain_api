ff8c09b5db20811163833c6090542e57e6299322
roy terrell <rmt2bsc@gmail.com>
2017-04-21 15:48:01 -0500

    Added mock test response data.

--- api_parent/api_addressbook/src/test/java/org/rmt2/api/profiles/business/BusinessProfileApiTest.java
+++ api_parent/api_addressbook/src/test/java/org/rmt2/api/profiles/business/BusinessProfileApiTest.java
@@ -23,9 +23,9 @@ import org.modules.contacts.ContactsApiException;
 import org.modules.contacts.ContactsApiFactory;
 import org.powermock.core.classloader.annotations.PrepareForTest;
 import org.powermock.modules.junit4.PowerMockRunner;
-import org.rmt2.api.BaseDaoTest;
 
 import com.api.persistence.AbstractDaoClientImpl;
+import com.api.persistence.BaseDaoTest;
 import com.api.persistence.db.orm.Rmt2OrmClientFactory;
 
 /**
@@ -37,12 +37,16 @@ import com.api.persistence.db.orm.Rmt2OrmClientFactory;
 public class BusinessProfileApiTest extends BaseDaoTest {
 
     private List<VwBusinessAddress> mockSingleProfileFetchResponse;
+    private List<VwBusinessAddress> mockCriteriaProfileFetchResponse;
+    private List<VwBusinessAddress> mockAllProfileFetchResponse;
 
     @Override
     public void setUp() throws Exception {
         APP_NAME = "addressbook";
         super.setUp();
         this.mockSingleProfileFetchResponse = this.createSingleContactFetchResponse();
+        this.mockCriteriaProfileFetchResponse = this.createContactFetchUsingCriteriaResponse();
+        this.mockAllProfileFetchResponse = this.createContactFetchAllResponse();
     }
 
     @Override
@@ -77,6 +81,121 @@ public class BusinessProfileApiTest extends BaseDaoTest {
         return list;
     }
 
+    /**
+     * Use for the following selection criteria: where longname begins with 'M'
+     * 
+     * @return
+     */
+    private List<VwBusinessAddress> createContactFetchUsingCriteriaResponse() {
+        List<VwBusinessAddress> list = new ArrayList<VwBusinessAddress>();
+        VwBusinessAddress b = new VwBusinessAddress();
+        b.setBusinessId(3333);
+        b.setBusLongname("Mondo");
+        b.setBusContactFirstname("Julianna");
+        b.setBusContactLastname("Young");
+        b.setBusContactPhone("2158882222");
+        b.setBusServTypeId(130);
+        b.setBusEntityTypeId(100);
+        b.setBusTaxId("94-7584738");
+        b.setBusWebsite("mondo.com");
+
+        b.setAddrId(1111);
+        b.setAddrBusinessId(3333);
+        b.setAddr1("8439 Elm St");
+        b.setAddr2("Suite 45");
+        b.setZipCity("dallas");
+        b.setZipState("TX");
+        b.setAddrZip(75232);
+        list.add(b);
+
+        b = new VwBusinessAddress();
+        b.setBusinessId(4444);
+        b.setBusLongname("Modis");
+        b.setBusContactFirstname("Allison");
+        b.setBusContactLastname("Wing");
+        b.setBusContactPhone("8175559999");
+        b.setBusServTypeId(130);
+        b.setBusEntityTypeId(100);
+        b.setBusTaxId("64-8888888");
+        b.setBusWebsite("modis.com");
+
+        b.setAddrId(55555);
+        b.setAddrBusinessId(4444);
+        b.setAddr1("77474 Heist Ave");
+        b.setZipCity("Boston");
+        b.setZipState("MA");
+        b.setAddrZip(65069);
+        list.add(b);
+
+        return list;
+    }
+
+    private List<VwBusinessAddress> createContactFetchAllResponse() {
+        List<VwBusinessAddress> list = new ArrayList<VwBusinessAddress>();
+        VwBusinessAddress b = new VwBusinessAddress();
+        b.setBusinessId(3333);
+        b.setBusLongname("Mondo");
+        b.setBusContactFirstname("Julianna");
+        b.setBusContactLastname("Young");
+        b.setBusContactPhone("2158882222");
+        b.setBusServTypeId(130);
+        b.setBusEntityTypeId(100);
+        b.setBusTaxId("94-7584738");
+        b.setBusWebsite("mondo.com");
+
+        b.setAddrId(1111);
+        b.setAddrBusinessId(3333);
+        b.setAddr1("8439 Elm St");
+        b.setAddr2("Suite 45");
+        b.setZipCity("dallas");
+        b.setZipState("TX");
+        b.setAddrZip(75232);
+        list.add(b);
+
+        b = new VwBusinessAddress();
+        b.setBusinessId(4444);
+        b.setBusLongname("Modis");
+        b.setBusContactFirstname("Allison");
+        b.setBusContactLastname("Wing");
+        b.setBusContactPhone("8175559999");
+        b.setBusServTypeId(130);
+        b.setBusEntityTypeId(100);
+        b.setBusTaxId("64-8888888");
+        b.setBusWebsite("modis.com");
+
+        b.setAddrId(55555);
+        b.setAddrBusinessId(4444);
+        b.setAddr1("77474 Heist Ave");
+        b.setZipCity("Boston");
+        b.setZipState("MA");
+        b.setAddrZip(65069);
+        list.add(b);
+
+        b = new VwBusinessAddress();
+        b.setBusinessId(1351);
+        b.setBusLongname("Ticket Master");
+        b.setBusContactFirstname("roy");
+        b.setBusContactLastname("terrell");
+        b.setBusContactPhone("9728882222");
+        b.setBusServTypeId(130);
+        b.setBusEntityTypeId(100);
+        b.setBusTaxId("75-9847382");
+        b.setBusWebsite("ticketmaster.com");
+
+        b.setAddrId(2222);
+        b.setAddrBusinessId(1351);
+        b.setAddr1("94393 Hall Ave.");
+        b.setAddr2("Suite 948");
+        b.setAddr3("P.O. Box 84763");
+        b.setZipCity("dallas");
+        b.setZipState("TX");
+        b.setAddrZip(75028);
+        b.setAddrZipext(1234);
+        list.add(b);
+
+        return list;
+
+    }
     @Test
     public void testSingleBusinessContactFetch() {
         Business bus = new Business();
