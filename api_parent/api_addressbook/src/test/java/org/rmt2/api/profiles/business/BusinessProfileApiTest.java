5191e34932cec14f7036347a14a46576ef764c27
roy terrell <rmt2bsc@gmail.com>
2017-04-22 12:39:13 -0500

    Completed the implementation of test cases that pertains to fetching business contacts.

--- api_parent/api_addressbook/src/test/java/org/rmt2/api/profiles/business/BusinessProfileApiTest.java
+++ api_parent/api_addressbook/src/test/java/org/rmt2/api/profiles/business/BusinessProfileApiTest.java
@@ -10,6 +10,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.dao.contacts.ContactDaoException;
+import org.dao.contacts.ContactsConst;
 import org.dao.mapping.orm.rmt2.Business;
 import org.dao.mapping.orm.rmt2.VwBusinessAddress;
 import org.dto.BusinessContactDto;
@@ -39,6 +40,7 @@ public class BusinessProfileApiTest extends BaseDaoTest {
     private List<VwBusinessAddress> mockSingleProfileFetchResponse;
     private List<VwBusinessAddress> mockCriteriaProfileFetchResponse;
     private List<VwBusinessAddress> mockAllProfileFetchResponse;
+    private List<VwBusinessAddress> mockNotFoundFetchResponse;
 
     @Override
     public void setUp() throws Exception {
@@ -47,11 +49,17 @@ public class BusinessProfileApiTest extends BaseDaoTest {
         this.mockSingleProfileFetchResponse = this.createSingleContactFetchResponse();
         this.mockCriteriaProfileFetchResponse = this.createContactFetchUsingCriteriaResponse();
         this.mockAllProfileFetchResponse = this.createContactFetchAllResponse();
+        this.mockNotFoundFetchResponse = this.createNotFoundSearchResultsResponse();
     }
 
     @Override
     public void tearDown() throws Exception {
         super.tearDown();
+    }
+
+    private List<VwBusinessAddress> createNotFoundSearchResultsResponse() {
+        List<VwBusinessAddress> list = new ArrayList<VwBusinessAddress>();
+        return list;
     }
 
     private List<VwBusinessAddress> createSingleContactFetchResponse() {
@@ -62,6 +70,7 @@ public class BusinessProfileApiTest extends BaseDaoTest {
         b.setBusContactFirstname("roy");
         b.setBusContactLastname("terrell");
         b.setBusContactPhone("9728882222");
+        b.setContactEmail("royterrell@gte.net");
         b.setBusServTypeId(130);
         b.setBusEntityTypeId(100);
         b.setBusTaxId("75-9847382");
@@ -94,6 +103,7 @@ public class BusinessProfileApiTest extends BaseDaoTest {
         b.setBusContactFirstname("Julianna");
         b.setBusContactLastname("Young");
         b.setBusContactPhone("2158882222");
+        b.setContactEmail("jules@gte.net");
         b.setBusServTypeId(130);
         b.setBusEntityTypeId(100);
         b.setBusTaxId("94-7584738");
@@ -114,6 +124,7 @@ public class BusinessProfileApiTest extends BaseDaoTest {
         b.setBusContactFirstname("Allison");
         b.setBusContactLastname("Wing");
         b.setBusContactPhone("8175559999");
+        b.setContactEmail("allison@gte.net");
         b.setBusServTypeId(130);
         b.setBusEntityTypeId(100);
         b.setBusTaxId("64-8888888");
@@ -138,6 +149,7 @@ public class BusinessProfileApiTest extends BaseDaoTest {
         b.setBusContactFirstname("Julianna");
         b.setBusContactLastname("Young");
         b.setBusContactPhone("2158882222");
+        b.setContactEmail("jules@gte.net");
         b.setBusServTypeId(130);
         b.setBusEntityTypeId(100);
         b.setBusTaxId("94-7584738");
@@ -158,6 +170,7 @@ public class BusinessProfileApiTest extends BaseDaoTest {
         b.setBusContactFirstname("Allison");
         b.setBusContactLastname("Wing");
         b.setBusContactPhone("8175559999");
+        b.setContactEmail("allison@gte.net");
         b.setBusServTypeId(130);
         b.setBusEntityTypeId(100);
         b.setBusTaxId("64-8888888");
@@ -177,6 +190,7 @@ public class BusinessProfileApiTest extends BaseDaoTest {
         b.setBusContactFirstname("roy");
         b.setBusContactLastname("terrell");
         b.setBusContactPhone("9728882222");
+        b.setContactEmail("royterrell@gte.net");
         b.setBusServTypeId(130);
         b.setBusEntityTypeId(100);
         b.setBusTaxId("75-9847382");
@@ -196,24 +210,16 @@ public class BusinessProfileApiTest extends BaseDaoTest {
         return list;
 
     }
+
     @Test
-    public void testSingleBusinessContactFetch() {
-        Business bus = new Business();
-        // Address addr = new Address();
-
-        bus.setBusinessId(1351);
-        // bus.setLongname("Ticket Master");
-
-        // addr.setAddrId(2258);
-        // addr.setBusinessId(1351);
-        // addr.setPhoneMain("2143738000");
-
-        BusinessContactDto busDto = Rmt2AddressBookDtoFactory.getBusinessInstance(bus, null);
+    public void testAllBusinessContactFetch() {
+        BusinessContactDto busDto = Rmt2AddressBookDtoFactory.getNewBusinessInstance();
         try {
             when(this.mockPersistenceClient.retrieveList(any(VwBusinessAddress.class)))
-                    .thenReturn(this.mockSingleProfileFetchResponse);
+                    .thenReturn(this.mockAllProfileFetchResponse);
         } catch (ContactDaoException e) {
-
+            e.printStackTrace();
+            Assert.fail("All business contact fetch test case failed");
         }
 
         ContactsApiFactory f = new ContactsApiFactory();
@@ -225,6 +231,135 @@ public class BusinessProfileApiTest extends BaseDaoTest {
             e.printStackTrace();
         }
         Assert.assertNotNull(results);
+        Assert.assertEquals(3, results.size());
+
+        for (ContactDto contact : results) {
+            Assert.assertNotNull(contact);
+            Assert.assertNotNull(contact.getContactType());
+            Assert.assertEquals(ContactsConst.CONTACT_TYPE_BUSINESS, contact.getContactType());
+            Assert.assertTrue(contact.getContactId() > 0);
+            Assert.assertNotNull(contact.getContactName());
+
+            Assert.assertTrue(contact instanceof BusinessContactDto);
+            BusinessContactDto busContact = (BusinessContactDto) contact;
+            Assert.assertNotNull(busContact.getContactFirstname());
+            Assert.assertNotNull(busContact.getContactLastname());
+            Assert.assertNotNull(busContact.getContactEmail());
+        }
     }
 
+    @Test
+    public void testSingleBusinessContactFetch() {
+        Business bus = new Business();
+        bus.setBusinessId(1351);
+        BusinessContactDto busDto = Rmt2AddressBookDtoFactory.getBusinessInstance(bus, null);
+        try {
+            when(this.mockPersistenceClient.retrieveList(any(VwBusinessAddress.class)))
+                    .thenReturn(this.mockSingleProfileFetchResponse);
+        } catch (ContactDaoException e) {
+            e.printStackTrace();
+            Assert.fail("Single business contact fetch test case failed");
+        }
+
+        ContactsApiFactory f = new ContactsApiFactory();
+        ContactsApi api = f.createApi(APP_NAME);
+        List<ContactDto> results = null;
+        try {
+            results = api.getContact(busDto);
+        } catch (ContactsApiException e) {
+            e.printStackTrace();
+        }
+        Assert.assertNotNull(results);
+        Assert.assertEquals(1, results.size());
+        ContactDto contact = results.get(0);
+        Assert.assertNotNull(contact.getContactType());
+        Assert.assertEquals(ContactsConst.CONTACT_TYPE_BUSINESS, contact.getContactType());
+        Assert.assertEquals(1351, contact.getContactId());
+        Assert.assertNotNull(contact.getContactName());
+        Assert.assertEquals("Ticket Master", contact.getContactName());
+
+        Assert.assertTrue(contact instanceof BusinessContactDto);
+        BusinessContactDto busContact = (BusinessContactDto) contact;
+        Assert.assertNotNull(busContact.getContactFirstname());
+        Assert.assertNotNull(busContact.getContactLastname());
+        Assert.assertNotNull(busContact.getContactEmail());
+
+    }
+
+    @Test
+    public void testBusinessContactCriteriaFetch() {
+        Business bus = new Business();
+        bus.setLongname("M");
+        BusinessContactDto busDto = Rmt2AddressBookDtoFactory.getBusinessInstance(bus, null);
+        try {
+            when(this.mockPersistenceClient.retrieveList(any(VwBusinessAddress.class)))
+                    .thenReturn(this.mockCriteriaProfileFetchResponse);
+        } catch (ContactDaoException e) {
+            e.printStackTrace();
+            Assert.fail("business contact fetch using specific selection criteria test case failed");
+        }
+
+        ContactsApiFactory f = new ContactsApiFactory();
+        ContactsApi api = f.createApi(APP_NAME);
+        List<ContactDto> results = null;
+        try {
+            results = api.getContact(busDto);
+        } catch (ContactsApiException e) {
+            e.printStackTrace();
+        }
+        Assert.assertNotNull(results);
+        Assert.assertEquals(2, results.size());
+        for (ContactDto contact : results) {
+            Assert.assertNotNull(contact);
+            Assert.assertNotNull(contact.getContactType());
+            Assert.assertEquals(ContactsConst.CONTACT_TYPE_BUSINESS, contact.getContactType());
+            Assert.assertTrue(contact.getContactId() > 0);
+            Assert.assertNotNull(contact.getContactName());
+            Assert.assertEquals("M", contact.getContactName().substring(0, 1));
+
+            Assert.assertTrue(contact instanceof BusinessContactDto);
+            BusinessContactDto busContact = (BusinessContactDto) contact;
+            Assert.assertNotNull(busContact.getContactFirstname());
+            Assert.assertNotNull(busContact.getContactLastname());
+            Assert.assertNotNull(busContact.getContactEmail());
+        }
+    }
+
+    @Test
+    public void testNotFoundlBusinessContactFetch() {
+        BusinessContactDto busDto = Rmt2AddressBookDtoFactory.getNewBusinessInstance();
+        try {
+            when(this.mockPersistenceClient.retrieveList(any(VwBusinessAddress.class)))
+                    .thenReturn(this.mockNotFoundFetchResponse);
+        } catch (ContactDaoException e) {
+            e.printStackTrace();
+            Assert.fail("Not Found business contact fetch test case failed");
+        }
+
+        ContactsApiFactory f = new ContactsApiFactory();
+        ContactsApi api = f.createApi(APP_NAME);
+        List<ContactDto> results = null;
+        try {
+            results = api.getContact(busDto);
+        } catch (ContactsApiException e) {
+            e.printStackTrace();
+        }
+        Assert.assertNotNull(results);
+        Assert.assertEquals(0, results.size());
+    }
+
+    @Test
+    public void testNullCriteriaBusinessContactFetch() {
+        ContactsApiFactory f = new ContactsApiFactory();
+        ContactsApi api = f.createApi(APP_NAME);
+        try {
+            api.getContact(null);
+            Assert.fail("Expected test case to throw an exception");
+        } catch (Exception e) {
+            String expectedMsg = "Error retrieving list of contacts using DTO as criteria";
+            Assert.assertTrue(e instanceof ContactDaoException);
+            Assert.assertEquals(expectedMsg, e.getMessage());
+            e.printStackTrace();
+        }
+    }
 }
