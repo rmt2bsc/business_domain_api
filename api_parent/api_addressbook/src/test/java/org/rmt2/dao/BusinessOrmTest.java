9acc2d6c9a050c2d533be4a699c543056adf41b1
roy terrell <rmt2bsc@gmail.com>
2017-05-14 15:04:00 -0500

    Created

---
+++ api_parent/api_addressbook/src/test/java/org/rmt2/dao/BusinessOrmTest.java
@@ -0,0 +1,85 @@
+package org.rmt2.dao;
+
+import org.dao.mapping.orm.rmt2.Business;
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+public class BusinessOrmTest {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+
+    @Test
+    public void testToString() {
+        Business o = new Business();
+        o.setBusinessId(1351);
+        o.setLongname("ABC Company");
+        o.setContactFirstname("Roy");
+        o.setContactLastname("Terrell");
+        o.setContactPhone("3189994444");
+        String val = o.toString();
+        System.out.println(val);
+        Assert.assertNotNull(val);
+    }
+
+    @Test
+    public void testEquality() {
+        boolean result = false;
+        Business o1 = new Business();
+        Business o2 = null;
+
+        result = o1.equals(o2);
+        Assert.assertFalse(result);
+
+        o1.setBusinessId(1351);
+        o1.setLongname("ABC Company");
+        o1.setContactFirstname("Roy");
+        o1.setContactLastname("Terrell");
+        o1.setContactPhone("3189994444");
+        o2 = new Business();
+
+        result = o1.equals(o2);
+        o2.setBusinessId(1351);
+        result = o1.equals(o2);
+        Assert.assertFalse(result);
+        o2.setLongname("ABC Company");
+        result = o1.equals(o2);
+        Assert.assertFalse(result);
+        o2.setContactFirstname("Roy");
+        result = o1.equals(o2);
+        Assert.assertFalse(result);
+        o2.setContactLastname("Terrell");
+        result = o1.equals(o2);
+        Assert.assertFalse(result);
+        o2.setContactPhone("3189994444");
+        result = o1.equals(o2);
+        Assert.assertTrue(result);
+    }
+
+    @Test
+    public void testHashCode() {
+        Business o1 = new Business();
+        o1.setBusinessId(1351);
+        o1.setLongname("ABC Company");
+        o1.setContactFirstname("Roy");
+        o1.setContactLastname("Terrell");
+        o1.setContactPhone("3189994444");
+
+        Business o2 = new Business();
+        o2.setBusinessId(1351);
+        o2.setLongname("ABC Company");
+        o2.setContactFirstname("Roy");
+        o2.setContactLastname("Terrell");
+        o2.setContactPhone("3189994444");
+
+        Assert.assertTrue(o1.equals(o2) && o2.equals(o1));
+        Assert.assertEquals(o1.hashCode(), o2.hashCode());
+    }
+}
---
+++ api_parent/api_addressbook/src/test/java/org/rmt2/dao/PersonOrmTest.java
@@ -0,0 +1,78 @@
+package org.rmt2.dao;
+
+import org.dao.mapping.orm.rmt2.Person;
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+public class PersonOrmTest {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+
+    @Test
+    public void testToString() {
+        Person o = new Person();
+        o.setPersonId(1351);
+        o.setFirstname("Roy");
+        o.setLastname("Terrell");
+        o.setEmail("roy.terrell@gte.net");
+        String val = o.toString();
+        System.out.println(val);
+        Assert.assertNotNull(val);
+    }
+
+    @Test
+    public void testEquality() {
+        boolean result = false;
+        Person o1 = new Person();
+        Person o2 = null;
+
+        result = o1.equals(o2);
+        Assert.assertFalse(result);
+
+        o1.setPersonId(1351);
+        o1.setFirstname("Roy");
+        o1.setLastname("Terrell");
+        o1.setEmail("roy.terrell@gte.net");
+        o2 = new Person();
+
+        result = o1.equals(o2);
+        o2.setPersonId(1351);
+        result = o1.equals(o2);
+        Assert.assertFalse(result);
+        o2.setFirstname("Roy");
+        result = o1.equals(o2);
+        Assert.assertFalse(result);
+        o2.setLastname("Terrell");
+        result = o1.equals(o2);
+        Assert.assertFalse(result);
+        o2.setEmail("roy.terrell@gte.net");
+        result = o1.equals(o2);
+        Assert.assertTrue(result);
+    }
+
+    @Test
+    public void testHashCode() {
+        Person o1 = new Person();
+        o1.setPersonId(1351);
+        o1.setFirstname("Roy");
+        o1.setLastname("Terrell");
+        o1.setEmail("roy.terrell@gte.net");
+
+        Person o2 = new Person();
+        o2.setPersonId(1351);
+        o2.setFirstname("Roy");
+        o2.setLastname("Terrell");
+        o2.setEmail("roy.terrell@gte.net");
+
+        Assert.assertTrue(o1.equals(o2) && o2.equals(o1));
+        Assert.assertEquals(o1.hashCode(), o2.hashCode());
+    }
+}
