f7d83aef013fa42e6a3a4fe6996c782d2380dde1
DHISCO\roy.terrell <roy.terrell@dhisco.com>
2017-06-08 12:14:31 -0500

    CA-10:  Moved ORM and DTO data mocking methods to AccountingMockDataUtility class.

--- api_parent/api_accounting/src/test/java/org/rmt2/api/generalledger/AccountApiTest.java
+++ api_parent/api_accounting/src/test/java/org/rmt2/api/generalledger/AccountApiTest.java
@@ -21,6 +21,7 @@ import org.modules.generalledger.GlAccountApi;
 import org.powermock.core.classloader.annotations.PrepareForTest;
 import org.powermock.modules.junit4.PowerMockRunner;
 import org.rmt2.api.BaseAccountingDaoTest;
+import org.rmt2.dao.AccountingMockDataUtility;
 
 import com.api.persistence.AbstractDaoClientImpl;
 import com.api.persistence.db.orm.Rmt2OrmClientFactory;
@@ -70,7 +71,7 @@ public class AccountApiTest extends BaseAccountingDaoTest {
 
     private List<GlAccounts> createMockSingleFetchResponse() {
         List<GlAccounts> list = new ArrayList<GlAccounts>();
-        GlAccounts p = this.createMockOrm(100, 200, 300, 1, "GL_100",
+        GlAccounts p = AccountingMockDataUtility.createMockOrm(100, 200, 300, 1, "GL_100",
                 "ACCT_RECV", "234", "Accounts Receivable", 1);
         list.add(p);
         return list;
@@ -83,11 +84,11 @@ public class AccountApiTest extends BaseAccountingDaoTest {
      */
     private List<GlAccounts> createMockFetchUsingCriteriaResponse() {
         List<GlAccounts> list = new ArrayList<GlAccounts>();
-        GlAccounts p = this.createMockOrm(100, 200, 300, 1, "GL_100",
+        GlAccounts p = AccountingMockDataUtility.createMockOrm(100, 200, 300, 1, "GL_100",
                 "ACCT_RECV", "234", "Accounts Receivable", 1);
         list.add(p);
 
-        p = this.createMockOrm(100, 200, 300, 1, "GL_101", "CASH", "230",
+        p = AccountingMockDataUtility.createMockOrm(100, 200, 300, 1, "GL_101", "CASH", "230",
                 "CASH", 1);
         list.add(p);
 
@@ -96,50 +97,23 @@ public class AccountApiTest extends BaseAccountingDaoTest {
 
     private List<GlAccounts> createMockFetchAllResponse() {
         List<GlAccounts> list = new ArrayList<GlAccounts>();
-        GlAccounts p = this.createMockOrm(100, 1, 120, 1, "GL_100", "ACCT_RECV",
+        GlAccounts p = AccountingMockDataUtility.createMockOrm(100, 1, 120, 1, "GL_100", "ACCT_RECV",
                 "134", "Accounts Receivable", 1);
         list.add(p);
 
-        p = this.createMockOrm(200, 2, 150, 1, "GL_201", "ACCT_PAY", "230",
+        p = AccountingMockDataUtility.createMockOrm(200, 2, 150, 1, "GL_201", "ACCT_PAY", "230",
                 "Accounts Payable", 2);
         list.add(p);
 
-        p = this.createMockOrm(300, 3, 160, 1, "GL_301", "OWNER_CAP", "330",
+        p = AccountingMockDataUtility.createMockOrm(300, 3, 160, 1, "GL_301", "OWNER_CAP", "330",
                 "Owner's Capital", 2);
         list.add(p);
 
-        p = this.createMockOrm(400, 4, 170, 1, "GL_401", "CRED_PURCH", "430",
+        p = AccountingMockDataUtility.createMockOrm(400, 4, 170, 1, "GL_401", "CRED_PURCH", "430",
                 "Creditor Purchases", 1);
         list.add(p);
         return list;
     }
-
-    private AccountDto createMockDto(int acctId, int acctTypeId, int acctCatgId,
-            int acctSeq, String acctNo, String acctName, String acctCode,
-            String acctDescription, int acctBalTypeId) {
-        GlAccounts orm = this.createMockOrm(acctId, acctTypeId, acctCatgId,
-                acctSeq, acctNo, acctName, acctCode, acctDescription,
-                acctBalTypeId);
-        AccountDto dto = Rmt2AccountDtoFactory.createAccountInstance(orm);
-        return dto;
-    }
-
-    private GlAccounts createMockOrm(int acctId, int acctTypeId, int acctCatgId,
-            int acctSeq, String acctNo, String acctName, String acctCode,
-            String acctDescription, int acctBalTypeId) {
-        GlAccounts orm = new GlAccounts();
-        orm.setAcctId(acctId);
-        orm.setAcctTypeId(acctTypeId);
-        orm.setAcctBaltypeId(acctBalTypeId);
-        orm.setAcctCatgId(acctCatgId);
-        orm.setAcctNo(acctNo);
-        orm.setAcctSeq(acctSeq);
-        orm.setCode(acctCode);
-        orm.setDescription(acctDescription);
-        orm.setName(acctName);
-        return orm;
-    }
-
 
     @Test
     public void testFetchAll() {
