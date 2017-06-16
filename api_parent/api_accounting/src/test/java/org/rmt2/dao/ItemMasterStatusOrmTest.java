ef72cbafac7b61b6998183f00e917a017ea24866
DHISCO\roy.terrell <roy.terrell@dhisco.com>
2017-06-16 15:18:28 -0500

    CA-11:  Completed developing  JUnit test cases for Item Master Status query

---
+++ api_parent/api_accounting/src/test/java/org/rmt2/dao/ItemMasterStatusOrmTest.java
@@ -0,0 +1,58 @@
+package org.rmt2.dao;
+
+import org.dao.mapping.orm.rmt2.ItemMasterStatus;
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+public class ItemMasterStatusOrmTest {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+
+    @Test
+    public void testToString() {
+        ItemMasterStatus o =  AccountingMockDataUtility.createMockOrmItemMasterStatus(100, "Description 1");
+        String val = o.toString();
+        System.out.println(val);
+        Assert.assertNotNull(val);
+    }
+
+    @Test
+    public void testEquality() {
+        boolean result = false;
+        ItemMasterStatus o1 = new ItemMasterStatus();
+        ItemMasterStatus o2 = null;
+
+        result = o1.equals(o2);
+        Assert.assertFalse(result);
+
+        o1 = AccountingMockDataUtility.createMockOrmItemMasterStatus(100, "Description 1");
+        o2 = new ItemMasterStatus();
+        result = o1.equals(o2);
+        Assert.assertFalse(result);
+        
+        o2.setItemStatusId(100);
+        result = o1.equals(o2);
+        Assert.assertFalse(result);
+   
+        o2.setDescription("Description 1");
+        result = o1.equals(o2);
+        Assert.assertTrue(result);
+    }
+
+    @Test
+    public void testHashCode() {
+        ItemMasterStatus o1 = AccountingMockDataUtility.createMockOrmItemMasterStatus(100, "Description 1");
+
+        ItemMasterStatus o2 = AccountingMockDataUtility.createMockOrmItemMasterStatus(100, "Description 1");
+        Assert.assertTrue(o1.equals(o2) && o2.equals(o1));
+        Assert.assertEquals(o1.hashCode(), o2.hashCode());
+    }
+}
---
+++ api_parent/api_accounting/src/test/java/org/rmt2/dao/ItemMasterTypeOrmTest.java
@@ -0,0 +1,58 @@
+package org.rmt2.dao;
+
+import org.dao.mapping.orm.rmt2.ItemMasterType;
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+public class ItemMasterTypeOrmTest {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+
+    @Test
+    public void testToString() {
+        ItemMasterType o =  AccountingMockDataUtility.createMockOrmItemMasterType(100, "Description 1");
+        String val = o.toString();
+        System.out.println(val);
+        Assert.assertNotNull(val);
+    }
+
+    @Test
+    public void testEquality() {
+        boolean result = false;
+        ItemMasterType o1 = new ItemMasterType();
+        ItemMasterType o2 = null;
+
+        result = o1.equals(o2);
+        Assert.assertFalse(result);
+
+        o1 = AccountingMockDataUtility.createMockOrmItemMasterType(100, "Description 1");
+        o2 = new ItemMasterType();
+        result = o1.equals(o2);
+        Assert.assertFalse(result);
+        
+        o2.setItemTypeId(100);
+        result = o1.equals(o2);
+        Assert.assertFalse(result);
+   
+        o2.setDescription("Description 1");
+        result = o1.equals(o2);
+        Assert.assertTrue(result);
+    }
+
+    @Test
+    public void testHashCode() {
+        ItemMasterType o1 = AccountingMockDataUtility.createMockOrmItemMasterType(100, "Description 1");
+
+        ItemMasterType o2 = AccountingMockDataUtility.createMockOrmItemMasterType(100, "Description 1");
+        Assert.assertTrue(o1.equals(o2) && o2.equals(o1));
+        Assert.assertEquals(o1.hashCode(), o2.hashCode());
+    }
+}
