c7cec1e1e3c47599b44118debcd14e494455dd56
DHISCO\roy.terrell <roy.terrell@dhisco.com>
2018-03-20 08:02:38 -0500

    CA-57: Added logic to test the manual setting of propertes for various Admin Module adapter methods.

--- api_parent/api_projecttracker/src/test/java/org/rmt2/entity/adapter/AdminModuleAdapterTest.java
+++ api_parent/api_projecttracker/src/test/java/org/rmt2/entity/adapter/AdminModuleAdapterTest.java
@@ -48,6 +48,35 @@ public class AdminModuleAdapterTest {
         Assert.assertEquals("gadd", dto.getClientContactLastname());
         Assert.assertEquals("0000000000", dto.getClientContactPhone());
         Assert.assertEquals("stevegadd@gte.net", dto.getClientContactEmail());
+        
+        try {
+            dto = ProjectObjectFactory.createClientDtoInstance(null);
+            dto.setClientId(1000);
+            dto.setBusinessId(1350);
+            dto.setAccountNo("acctNo");
+            dto.setClientName("CompanyName");
+            dto.setClientBillRate(70.00);
+            dto.setClientOtBillRate(80.00);
+            dto.setClientContactFirstname("steve");
+            dto.setClientContactLastname("gadd");
+            dto.setClientContactPhone("0000000000");
+            dto.setClientContactEmail("stevegadd@gte.net");
+            
+            Assert.assertEquals(1000, dto.getClientId());
+            Assert.assertEquals(1350, dto.getBusinessId());
+            Assert.assertEquals("acctNo", dto.getAccountNo());
+            Assert.assertEquals("CompanyName", dto.getClientName());
+            Assert.assertEquals(70.00, dto.getClientBillRate(), 0);
+            Assert.assertEquals(80.00, dto.getClientOtBillRate(), 0);
+            Assert.assertEquals("steve", dto.getClientContactFirstname());
+            Assert.assertEquals("gadd", dto.getClientContactLastname());
+            Assert.assertEquals("0000000000", dto.getClientContactPhone());
+            Assert.assertEquals("stevegadd@gte.net", dto.getClientContactEmail());
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+            Assert.fail("An exception occurred setting properties for ProjClient Adapater");
+        }
     }
     
     @Test
@@ -61,6 +90,25 @@ public class AdminModuleAdapterTest {
         Assert.assertEquals("ProjectDescription", dto.getProjectDescription());
         Assert.assertEquals(RMT2Date.stringToDate("2018-01-01"), dto.getProjectEffectiveDate());
         Assert.assertEquals(RMT2Date.stringToDate("2018-02-02"), dto.getProjectEndDate());
+        
+        try {
+            dto = ProjectObjectFactory.createProjectDtoInstance(null);
+            dto.setProjId(1000);
+            dto.setClientId(2000);
+            dto.setProjectDescription("ProjectDescription");
+            dto.setProjectEffectiveDate(RMT2Date.stringToDate("2018-01-01"));
+            dto.setProjectEndDate(RMT2Date.stringToDate("2018-02-02"));
+            
+            Assert.assertEquals(1000, dto.getProjId());
+            Assert.assertEquals(2000, dto.getClientId());
+            Assert.assertEquals("ProjectDescription", dto.getProjectDescription());
+            Assert.assertEquals(RMT2Date.stringToDate("2018-01-01"), dto.getProjectEffectiveDate());
+            Assert.assertEquals(RMT2Date.stringToDate("2018-02-02"), dto.getProjectEndDate());
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+            Assert.fail("An exception occurred setting properties for ProjProject Adapater");
+        }
     }
     
     @Test
@@ -71,5 +119,20 @@ public class AdminModuleAdapterTest {
         Assert.assertEquals(1000, dto.getTaskId());
         Assert.assertEquals("TaskDescription", dto.getTaskDescription());
         Assert.assertEquals(1, dto.getTaskBillable());
+        
+        try {
+            dto = ProjectObjectFactory.createTaskDtoInstance(null);
+            dto.setTaskId(1000);
+            dto.setTaskDescription("TaskDescription");
+            dto.setTaskBillable(1);
+            
+            Assert.assertEquals(1000, dto.getTaskId());
+            Assert.assertEquals("TaskDescription", dto.getTaskDescription());
+            Assert.assertEquals(1, dto.getTaskBillable());
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+            Assert.fail("An exception occurred setting properties for ProjTask Adapater");
+        }
     }
 }
