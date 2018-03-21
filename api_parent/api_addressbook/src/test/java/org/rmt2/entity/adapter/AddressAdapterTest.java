14c0fb9bb658251cb5b1dd8e6cd083a2e8554e63
DHISCO\roy.terrell <roy.terrell@dhisco.com>
2018-03-21 14:10:39 -0500

    CA-57: Removed private methods that creates mock data.   Instead used class, ContactProfileTestData, to obtain mock data.

--- api_parent/api_addressbook/src/test/java/org/rmt2/entity/adapter/AddressAdapterTest.java
+++ api_parent/api_addressbook/src/test/java/org/rmt2/entity/adapter/AddressAdapterTest.java
@@ -1,6 +1,5 @@
 package org.rmt2.entity.adapter;
 
-import org.dao.contacts.ContactsConst;
 import org.dao.mapping.orm.rmt2.Address;
 import org.dao.mapping.orm.rmt2.VwBusinessAddress;
 import org.dao.mapping.orm.rmt2.VwCommonContact;
@@ -14,6 +13,7 @@ import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
+import org.rmt2.api.profiles.ContactProfileTestData;
 
 /**
  * Test GL Account related adapters
@@ -31,95 +31,11 @@ public class AddressAdapterTest {
     public void tearDown() throws Exception {
     }
 
-    private Address createAddressMockObject() {
-        Address a = new Address();
-        a.setAddrId(2222);
-        a.setBusinessId(1351);
-        a.setAddr1("94393 Hall Ave.");
-        a.setAddr2("Suite 948");
-        a.setAddr3("Room 4");
-        a.setAddr4("P.O. Box 84763");
-        a.setZip(75028);
-        a.setZipext(1234);
-        return a;
-    }
     
-    private VwCommonContact createCommonContactAddressMockObject() {
-        VwCommonContact a = new VwCommonContact();
-        a.setContactId(1351);
-        a.setContactName("Dennis Chambers");
-        a.setContactType(ContactsConst.CONTACT_TYPE_PERSONAL);
-
-        a.setAddrId(2222);
-        a.setContactId(1351);
-        a.setAddr1("94393 Hall Ave.");
-        a.setAddr2("Suite 948");
-        a.setAddr3("Room 4");
-        a.setAddr4("P.O. Box 84763");
-        a.setZipCity("dallas");
-        a.setZipState("TX");
-        a.setAddrZip(75028);
-        a.setAddrZipext(1234);
-
-        return a;
-    }
-    
-    private VwBusinessAddress createBusinessAddressMockObject() {
-        VwBusinessAddress b = new VwBusinessAddress();
-        b.setBusinessId(1351);
-        b.setBusLongname("Ticket Master");
-        b.setBusContactFirstname("roy");
-        b.setBusContactLastname("terrell");
-        b.setBusContactPhone("9728882222");
-        b.setContactEmail("royterrell@gte.net");
-        b.setBusServTypeId(130);
-        b.setBusEntityTypeId(100);
-        b.setBusTaxId("75-9847382");
-        b.setBusWebsite("ticketmaster.com");
-
-        b.setAddrId(2222);
-        b.setAddrBusinessId(1351);
-        b.setAddr1("94393 Hall Ave.");
-        b.setAddr2("Suite 948");
-        b.setAddr3("Room 4");
-        b.setAddr4("P.O. Box 84763");
-        b.setZipCity("dallas");
-        b.setZipState("TX");
-        b.setAddrZip(75028);
-        b.setAddrZipext(1234);
-
-        return b;
-    }
-    
-    private VwPersonAddress createPersonAddressMockObject() {
-        VwPersonAddress p = new VwPersonAddress();
-        p.setPersonId(1351);
-        p.setPerFirstname("Dennis");
-        p.setPerLastname("Chambers");
-        p.setPerMidname("Milton");
-        p.setPerTitle(100);
-        p.setPerEmail("royterrell@gte.net");
-        p.setPerMaritalStatus(20);
-        p.setPerGenderId(12);
-        p.setPerSsn("345-59-4938");
-        p.setPerRaceId(30);
-
-        p.setAddrId(2222);
-        p.setAddrBusinessId(1351);
-        p.setAddr1("94393 Hall Ave.");
-        p.setAddr2("Suite 948");
-        p.setAddr3("Room 4");
-        p.setAddr4("P.O. Box 84763");
-        p.setZipCity("dallas");
-        p.setZipState("TX");
-        p.setAddrZip(75028);
-        p.setAddrZipext(1234);
-        return p;
-    }
     
     @Test
     public void testAddresAdapter() {
-        Address addr = this.createAddressMockObject();
+        Address addr = ContactProfileTestData.createAddressMockObject();
         AddressDto dto = Rmt2AddressBookDtoFactory.getAddressDtoInstance(addr);
         
         Assert.assertEquals(2222, dto.getAddrId());
@@ -156,7 +72,7 @@ public class AddressAdapterTest {
     
     @Test
     public void testCommonContactAddressAdapter() {
-        VwCommonContact o = this.createCommonContactAddressMockObject(); 
+        VwCommonContact o = ContactProfileTestData.createCommonContactAddressMockObject(); 
         ContactDto dto = Rmt2AddressBookDtoFactory.getContactInstance(o);
         
         Assert.assertEquals(2222, dto.getAddrId());
@@ -199,7 +115,7 @@ public class AddressAdapterTest {
     
     @Test
     public void testBusinessAddressAdapter() {
-        VwBusinessAddress o = this.createBusinessAddressMockObject();
+        VwBusinessAddress o = ContactProfileTestData.createBusinessAddressMockObject();
         BusinessContactDto dto = Rmt2AddressBookDtoFactory.getBusinessInstance(o);
         
         Assert.assertEquals(2222, dto.getAddrId());
@@ -241,7 +157,7 @@ public class AddressAdapterTest {
     
     @Test
     public void testPersonAddressAdapter() {
-        VwPersonAddress o = this.createPersonAddressMockObject();
+        VwPersonAddress o = ContactProfileTestData.createPersonAddressMockObject();
         PersonalContactDto dto = Rmt2AddressBookDtoFactory.getPersonInstance(o);
         
         Assert.assertEquals(2222, dto.getAddrId());
