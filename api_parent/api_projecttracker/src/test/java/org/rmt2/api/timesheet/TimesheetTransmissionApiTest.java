0fbb3bf61f85e3da2e9480c12546927a5e019e13
DHISCO\roy.terrell <roy.terrell@dhisco.com>
2018-04-04 18:06:50 -0500

    CA-22: Began coding test case for create timesheet confirmation success path.

--- api_parent/api_projecttracker/src/test/java/org/rmt2/api/timesheet/TimesheetTransmissionApiTest.java
+++ api_parent/api_projecttracker/src/test/java/org/rmt2/api/timesheet/TimesheetTransmissionApiTest.java
@@ -1,18 +1,32 @@
 package org.rmt2.api.timesheet;
 
-import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
+import org.dao.mapping.orm.rmt2.ProjEvent;
+import org.dao.mapping.orm.rmt2.ProjProjectTask;
+import org.dto.ClientDto;
+import org.dto.EmployeeDto;
+import org.dto.EventDto;
+import org.dto.ProjectTaskDto;
+import org.dto.TimesheetDto;
+import org.dto.adapter.orm.EmployeeObjectFactory;
+import org.dto.adapter.orm.ProjectObjectFactory;
+import org.dto.adapter.orm.TimesheetObjectFactory;
 import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.modules.timesheet.TimesheetApiFactory;
+import org.modules.timesheet.TimesheetTransmissionApi;
+import org.modules.timesheet.TimesheetTransmissionException;
 import org.powermock.core.classloader.annotations.PrepareForTest;
 import org.powermock.modules.junit4.PowerMockRunner;
 
-import com.api.persistence.AbstractDaoClientImpl;
-import com.api.persistence.db.orm.Rmt2OrmClientFactory;
+import com.api.messaging.email.smtp.SmtpFactory;
 
 /**
  * Test the timesheet transmission functionality of the Timesheet module of the
@@ -22,8 +36,7 @@ import com.api.persistence.db.orm.Rmt2OrmClientFactory;
  * 
  */
 @RunWith(PowerMockRunner.class)
-@PrepareForTest({ AbstractDaoClientImpl.class, Rmt2OrmClientFactory.class,
-        ResultSet.class, TimesheetApiFactory.class })
+@PrepareForTest({ SmtpFactory.class })
 public class TimesheetTransmissionApiTest extends TimesheetMockData {
 
     /**
@@ -43,11 +56,46 @@ public class TimesheetTransmissionApiTest extends TimesheetMockData {
         return;
     }
 
+    
+    private Map<ProjectTaskDto, List<EventDto>> buildTimesheetHoursDtoMap() {
+        Map<ProjectTaskDto, List<EventDto>> hours = new HashMap<>(); 
+        for (ProjProjectTask pt : this.mockProjProjectTaskMultiple) {
+            ProjectTaskDto ptDto = ProjectObjectFactory.createProjectTaskDtoInstance(pt);
+            List<EventDto> eventsDto = this.buildTimesheetEventDtoList(pt.getProjectTaskId());
+            hours.put(ptDto, eventsDto);
+        }
+        return hours;
+    }
+    
+    private List<EventDto> buildTimesheetEventDtoList(int projectTaskId) {
+        List<ProjEvent> projEvents = createMockMultiple_Day_Task_Events(projectTaskId);
+        List<EventDto> eventsDto = new ArrayList<>();
+        for (ProjEvent evt : projEvents) {
+            EventDto evtDto = ProjectObjectFactory.createEventDtoInstance(evt);
+            eventsDto.add(evtDto);
+        }
+        return eventsDto;
+    }
+    
+
     @Test
     public void testSuccess_CreateConfirmation() {
         // TODO: Please look at the success scenario of "submit" timesheet in
-        // TimesheetUpdateApiTest.java for mockin examples.
-        Assert.fail("Implement test case");
+        // TimesheetUpdateApiTest.java for mocking examples.
+
+        ClientDto client = ProjectObjectFactory.createClientDtoInstance(this.mockClientFetchMultiple.get(0));
+        EmployeeDto employee = EmployeeObjectFactory.createEmployeeDtoInstance(this.mockEmployeeFetchSingle.get(0));
+        EmployeeDto manager = EmployeeObjectFactory.createEmployeeDtoInstance(this.mockManagerFetchSingle.get(0));
+        TimesheetDto timesheet = TimesheetObjectFactory.createTimesheetDtoInstance(this.mockProjTimesheetSingle.get(0));
+        Map<ProjectTaskDto, List<EventDto>> hours = this.buildTimesheetHoursDtoMap();
+        
+        TimesheetTransmissionApi api = TimesheetApiFactory.createTransmissionApi();
+        try {
+            api.createConfirmationMessage(timesheet, employee, manager, client, hours);
+        }
+        catch (TimesheetTransmissionException e) {
+            
+        }
     }
 
     @Test
