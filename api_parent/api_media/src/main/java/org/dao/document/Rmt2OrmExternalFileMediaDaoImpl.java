b5089a2333c7431a648eced39b9060540ce61b57
DHISCO\roy.terrell <roy.terrell@dhisco.com>
2018-04-16 18:12:06 -0500

    CA-26: Made major source code changes that will allow data streams to be written to disc properly.

--- api_parent/api_media/src/main/java/org/dao/document/Rmt2OrmExternalFileMediaDaoImpl.java
+++ api_parent/api_media/src/main/java/org/dao/document/Rmt2OrmExternalFileMediaDaoImpl.java
@@ -2,20 +2,12 @@ package org.dao.document;
 
 import java.io.File;
 import java.io.FileInputStream;
-import java.util.List;
 
 import org.apache.log4j.Logger;
-import org.dao.mapping.orm.rmt2.Content;
 import org.dto.ContentDto;
-import org.dto.MimeTypeDto;
-import org.dto.adapter.orm.Rmt2MediaDtoFactory;
 
-import com.util.RMT2Base64Decoder;
 import com.util.RMT2Base64Encoder;
-import com.util.RMT2Date;
 import com.util.RMT2File;
-import com.util.RMT2String;
-import com.util.UserTimestamp;
 
 /**
  * File system database implementation of the {@link ContentDao} interface to
@@ -25,7 +17,7 @@ import com.util.UserTimestamp;
  * @author Roy Terrell
  * 
  */
-class Rmt2OrmExternalFileMediaDaoImpl extends AbstractRmt2OrmContentDaoImpl implements ContentDao {
+class Rmt2OrmExternalFileMediaDaoImpl extends AbstractRmt2OrmContentDaoImpl {
 
     private static Logger logger = Logger.getLogger(Rmt2OrmExternalFileMediaDaoImpl.class);
 
@@ -46,103 +38,6 @@ class Rmt2OrmExternalFileMediaDaoImpl extends AbstractRmt2OrmContentDaoImpl impl
         return;
     }
 
-    /**
-     * Creates media document as a combination of a database row and an external
-     * file.
-     * <p>
-     * An entry is made to the <i>content</i> table without storing the actual
-     * media document in the imageData column. Instead, the media document is
-     * stored as an external file in the file location designated as the <i>MIME
-     * archive directory</i>. The <i>MIME archive directory</i> is configured in
-     * the <i>MimeConfig_XXX.properties</i> file.
-     * <p>
-     * The user of this method is required to pass the image data as a Base64
-     * encoded String in order to prevent any possible data loss in the event
-     * the data is transported bewteen different processes. For instance, from
-     * one web application context to another.
-     * 
-     * @param mediaRec
-     *            an instance of {@link ContentDto} containing the data to add
-     *            including the large binary and/or text data.
-     * @return int the new content id just added.
-     * @throws ContentDaoException
-     *             <ul>
-     *             <li>The <i>mediaRec</i> does not meet validation rules.</li>
-     *             <li>Unable to create a row in the <i>content</i> table.</li>
-     *             <li>Media content could not be saved as an external file.
-     *             </li>
-     *             </ul>
-     */
-    public int addContent(ContentDto mediaRec) throws ContentDaoException {
-        logger.info("Validate Content record");
-        try {
-            this.validate(mediaRec);
-        } catch (Exception e) {
-            throw new ContentDaoException(e);
-        }
-
-        // We want to save the media content to the "archive" directory as well
-        // as have
-        // the media content metadata record point to the archive direcotry.
-        mediaRec.setFilepath(this.config.getArchiveDir());
-
-        logger.info("Begin to persisting media content record with external media file");
-        int contentId = this.saveMetaData(mediaRec);
-        try {
-            int rc = this.saveContentAsFile(mediaRec);
-            logger.info("Total number of bytes written to disk for media file, "
-                    + mediaRec.getFilename() + ": " + rc);
-        } catch (Exception e) {
-            throw new ContentDaoException(e);
-        }
-        return contentId;
-    }
-
-    /**
-     * Adds a record to the <i>content</i> table without including the image
-     * data (binary or text).
-     * <p>
-     * The filepath and filename properties of <i>mediaRec</i> should point the
-     * the directory location and the file name where the media document will be
-     * persisted, respectively.
-     * 
-     * @param mediaRec
-     *            An instance of {@link ContentDto}
-     * @return The primary key of the row just added to the <i>content</i>
-     *         table.
-     * @throws ContentDaoException
-     *             general database access error
-     */
-    protected int saveMetaData(ContentDto mediaRec) throws ContentDaoException {
-        mediaRec.setUpdateUserId(this.getDaoUser());
-
-        Content rec = new Content();
-        rec.setMimeTypeId(mediaRec.getMimeTypeId());
-        rec.setFilepath(mediaRec.getFilepath());
-        rec.setFilename(mediaRec.getFilename());
-        rec.setSize(mediaRec.getSize());
-        rec.setImageData(null);
-        rec.setTextData(null);
-        rec.setAppCode(null);
-        rec.setModuleCode(null);
-
-        int newContentId = 0;
-        this.client.beginTrans();
-        try {
-            UserTimestamp ut = RMT2Date.getUserTimeStamp(mediaRec.getUpdateUserId());
-            rec.setDateCreated(ut.getDateCreated());
-            rec.setUserId(ut.getLoginId());
-            rec.setNull(Content.PROP_PROJECTID);
-            newContentId = this.client.insertRow(rec, true);
-            mediaRec.setContentId(newContentId);
-            this.client.commitTrans();
-            return newContentId;
-        } catch (Exception e) {
-            this.client.rollbackTrans();
-            this.msg = "Error adding media record without embedded content";
-            throw new ContentDaoException(this.msg, e);
-        }
-    }
 
     /**
      * Fetches a row from the <i>content</i> table by its primary key where the
@@ -215,69 +110,12 @@ class Rmt2OrmExternalFileMediaDaoImpl extends AbstractRmt2OrmContentDaoImpl impl
             this.msg = "Unable to locate external media file, " + file.getAbsolutePath();
             throw new ContentDaoException(this.msg);
         }
+        
+        // TODO: need to obtain the file contents as byte array
         mime.setImageData(file);
         return mime;
     }
 
-    /**
-     * Verifies that the media content record contains valid data..
-     * <p>
-     * It is required that the file name and the image data is not null, and the
-     * image data must be a Base64 encoded String. Also, validates whether the
-     * file extension is registered in the database. If the MIME type exists,
-     * then the MIME type id is assigned to the mimeTypeId property of
-     * <i>dto</i>.
-     * <p>
-     * By default the filepath property will be assigned the file path of the
-     * archive directory as declared in <i>MimeConfig_XXX.properties</i> file.
-     * 
-     * @param dto
-     *            an instance of {@link ContentDto} which will be validated.
-     * @return Always return null.
-     * @throws ContentDaoException
-     *             The file name or the image data of <i>dto</i> is null, the
-     *             image data is not Base 64 encoded, or the file extension is
-     *             not an extension that exists in the database.
-     */
-    protected File validate(ContentDto dto) throws ContentDaoException {
-        String inFileName = dto.getFilename();
-        if (inFileName == null || inFileName.equals(RMT2String.spaces(inFileName.length()))) {
-            this.msg = "The filename property is required";
-            throw new ContentValidationDaoException(this.msg);
-        }
-        if (dto.getImageData() == null) {
-            this.msg = "The data (binary or text) representing the media content, that is to be persisted as an external file, cannot be null";
-            throw new ContentValidationDaoException(this.msg);
-        }
-        if (!(dto.getImageData() instanceof String)) {
-            this.msg = "The data (binary or text) representing the media content, that is to be persisted as an external file, must be a base 64 encoded String";
-            throw new ContentValidationDaoException(this.msg);
-        }
-
-        // dto.setFilepath(this.config.getArchiveDir());
-
-        logger.info("Validate file extension");
-        String ext = RMT2File.getFileExt(dto.getFilename());
-        if (ext == null) {
-            this.msg = "File name is required to have an extension when persisting an external media file";
-            throw new ContentValidationDaoException(this.msg);
-        }
-
-        // Get mime type id of file name.
-        MimeTypeDto mtCriteria = Rmt2MediaDtoFactory.getMimeTypeInstance(null);
-        mtCriteria.setFileExt(ext);
-        List<MimeTypeDto> list = this.fetchMimeType(mtCriteria);
-        if (list == null) {
-            this.msg = "File, "
-                    + dto.getFilename()
-                    + ", was not persisted due to file extension is not registered in the MIME database";
-            throw new ContentValidationDaoException(this.msg);
-        }
-        MimeTypeDto mt = list.get(0);
-        dto.setMimeTypeId(mt.getMimeTypeId());
-        logger.info("Passed Sysbase Adaptive Server Anywhere media file validations");
-        return null;
-    }
 
     /**
      * Saves the media content data as file in the archive directory location
@@ -292,10 +130,11 @@ class Rmt2OrmExternalFileMediaDaoImpl extends AbstractRmt2OrmContentDaoImpl impl
      *            be saved.
      * @return the total number of bytes saved to disk.
      */
-    protected int saveContentAsFile(ContentDto mediaRec) {
+    @Override
+    public int saveContent(ContentDto mediaRec) {
         Thread t = new Thread(new MediaFilePeristenceThread(mediaRec));
         t.start();
-        return 0;
+        return mediaRec.getSize();
     }
 
     /**
@@ -328,8 +167,9 @@ class Rmt2OrmExternalFileMediaDaoImpl extends AbstractRmt2OrmContentDaoImpl impl
             int bytesWritten = 0;
             if (this.rec.getImageData() != null) {
                 path = this.rec.getFilepath() + this.rec.getFilename();
-                byte binaryData[] = RMT2Base64Decoder.decodeToBytes(this.rec.getImageData().toString());
+                byte binaryData[] = (byte[]) rec.getImageData(); 
                 bytesWritten = RMT2File.outputFile(binaryData, path);
+                System.out.println("Total bytes written to path, " + path + ", from thread: " + bytesWritten );
                 this.rec.setSize(bytesWritten);
                 msg = "External media file, "
                         + this.rec.getFilename()
@@ -355,4 +195,5 @@ class Rmt2OrmExternalFileMediaDaoImpl extends AbstractRmt2OrmContentDaoImpl impl
             }
         }
     }
+    
 }
