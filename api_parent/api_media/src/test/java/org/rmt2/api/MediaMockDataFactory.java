1500210373f8cddd6009934277facb3655ced296
DHISCO\roy.terrell <roy.terrell@dhisco.com>
2018-04-17 17:35:42 -0500

    CA-26: Developed adapter and ORM JUnit tests to cover MimTypes and Content ORM classes.

--- api_parent/api_media/src/test/java/org/rmt2/api/MediaMockDataFactory.java
+++ api_parent/api_media/src/test/java/org/rmt2/api/MediaMockDataFactory.java
@@ -1,5 +1,6 @@
 package org.rmt2.api;
 
+import org.dao.mapping.orm.rmt2.Content;
 import org.dao.mapping.orm.rmt2.MimeTypes;
 
 public class MediaMockDataFactory {
@@ -17,6 +18,35 @@ public class MediaMockDataFactory {
         o.setFileExt(fileExt);
         o.setMediaType(mimeType);
         o.setMimeTypeId(id);
+        return o;
+    }
+    
+    /**
+     * 
+     * @param contentId
+     * @param mimeTypeId
+     * @param filepath
+     * @param filename
+     * @param size
+     * @param projectId
+     * @param appCode
+     * @return
+     */
+    public static final Content createOrmContent(int contentId, int mimeTypeId,
+            String filepath, String filename, int size, int projectId,
+            String appCode) {
+
+        Content o = new Content();
+        o.setContentId(contentId);
+        o.setMimeTypeId(mimeTypeId);
+        o.setFilepath(filepath);
+        o.setFilename(filename);
+        o.setSize(size);
+        o.setProjectId(projectId);
+        o.setAppCode(appCode);
+        o.setModuleCode("ModuleCode");
+        o.setTextData("TextData");
+        o.setImageData("ImageData");
         return o;
     }
 }
