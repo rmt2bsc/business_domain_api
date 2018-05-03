5406f359b4f52795e0ee7299de868fc8bcc76eea
appdev <royterrell2@gmail.com>
2018-05-03 10:24:01 -0500

    CA-25:  Added adapter test for Audio/Video Project ORM bean

--- api_parent/api_media/src/test/java/org/rmt2/api/entity/adapter/AudioVideoMediaAdapterTest.java
+++ api_parent/api_media/src/test/java/org/rmt2/api/entity/adapter/AudioVideoMediaAdapterTest.java
@@ -3,10 +3,12 @@ package org.rmt2.api.entity.adapter;
 import org.dao.mapping.orm.rmt2.AvArtist;
 import org.dao.mapping.orm.rmt2.AvGenre;
 import org.dao.mapping.orm.rmt2.AvMediaType;
+import org.dao.mapping.orm.rmt2.AvProject;
 import org.dao.mapping.orm.rmt2.AvProjectType;
 import org.dto.ArtistDto;
 import org.dto.GenreDto;
 import org.dto.MediaTypeDto;
+import org.dto.ProjectDto;
 import org.dto.ProjectTypeDto;
 import org.dto.adapter.orm.Rmt2MediaDtoFactory;
 import org.junit.After;
@@ -125,6 +127,67 @@ public class AudioVideoMediaAdapterTest {
         } catch (Exception e) {
             e.printStackTrace();
             Assert.fail("An exception occurred setting properties for Artist Adapater");
+        }
+    }
+
+    @Test
+    public void testOrmProject() {
+        AvProject o1 = AvMediaMockDataFactory.createOrmAvProject(AvMediaMockDataFactory.TEST_PROJECT_ID,
+                AvMediaMockDataFactory.TEST_ARTIST_ID, AvMediaMockDataFactory.TEST_PROJECTTYPE_ID,
+                AvMediaMockDataFactory.TEST_GENRE_ID, AvMediaMockDataFactory.TEST_MEDIA_TYPE_ID, "Title"
+                        + AvMediaMockDataFactory.TEST_PROJECT_ID, 2018, "/FilePath/"
+                        + AvMediaMockDataFactory.TEST_PROJECT_ID, "ProjectFileName"
+                        + AvMediaMockDataFactory.TEST_PROJECT_ID);
+        ProjectDto dto = Rmt2MediaDtoFactory.getAvProjectInstance(o1);
+
+        Assert.assertEquals(AvMediaMockDataFactory.TEST_ARTIST_ID, dto.getArtistId());
+        Assert.assertEquals(AvMediaMockDataFactory.TEST_PROJECT_ID, dto.getProjectId());
+        Assert.assertEquals(AvMediaMockDataFactory.TEST_PROJECTTYPE_ID, dto.getProjectTypeId());
+        Assert.assertEquals(AvMediaMockDataFactory.TEST_GENRE_ID, dto.getGenreId());
+        Assert.assertEquals(AvMediaMockDataFactory.TEST_MEDIA_TYPE_ID, dto.getMediaTypeId());
+        Assert.assertEquals("Title" + AvMediaMockDataFactory.TEST_PROJECT_ID, dto.getTitle());
+        Assert.assertEquals(2018, dto.getYear());
+        Assert.assertEquals("/FilePath/" + AvMediaMockDataFactory.TEST_PROJECT_ID, dto.getArtWorkPath());
+        Assert.assertEquals("ProjectFileName" + AvMediaMockDataFactory.TEST_PROJECT_ID, dto.getArtWorkFilename());
+        Assert.assertEquals(12.99, dto.getCost(), 0);
+        Assert.assertEquals("ProjectCommentsFor" + AvMediaMockDataFactory.TEST_PROJECT_ID, dto.getComments());
+        Assert.assertEquals(1, dto.getRippedInd());
+        Assert.assertEquals(AvMediaMockDataFactory.TEST_PROJECT_ID + 1000, dto.getMasterDupId());
+
+        try {
+            AvProject nullParm = null;
+            dto = Rmt2MediaDtoFactory.getAvProjectInstance(nullParm);
+            dto.setProjectId(AvMediaMockDataFactory.TEST_PROJECT_ID);
+            dto.setArtistId(AvMediaMockDataFactory.TEST_ARTIST_ID);
+            dto.setProjectTypeId(AvMediaMockDataFactory.TEST_PROJECTTYPE_ID);
+            dto.setGenreId(AvMediaMockDataFactory.TEST_GENRE_ID);
+            dto.setMediaTypeId(AvMediaMockDataFactory.TEST_MEDIA_TYPE_ID);
+            dto.setTitle("Title" + AvMediaMockDataFactory.TEST_PROJECT_ID);
+            dto.setYear(2018);
+            dto.setArtWorkPath("/FilePath/" + AvMediaMockDataFactory.TEST_PROJECT_ID);
+            dto.setArtWorkFilename("ProjectFileName" + AvMediaMockDataFactory.TEST_PROJECT_ID);
+            dto.setCost(12.99);
+            dto.setComments("ProjectCommentsFor" + AvMediaMockDataFactory.TEST_PROJECT_ID);
+            dto.setRippedInd(1);
+            dto.setMasterDupId(AvMediaMockDataFactory.TEST_PROJECT_ID + 1000);
+
+            Assert.assertEquals(AvMediaMockDataFactory.TEST_ARTIST_ID, dto.getArtistId());
+            Assert.assertEquals(AvMediaMockDataFactory.TEST_PROJECT_ID, dto.getProjectId());
+            Assert.assertEquals(AvMediaMockDataFactory.TEST_PROJECTTYPE_ID, dto.getProjectTypeId());
+            Assert.assertEquals(AvMediaMockDataFactory.TEST_GENRE_ID, dto.getGenreId());
+            Assert.assertEquals(AvMediaMockDataFactory.TEST_MEDIA_TYPE_ID, dto.getMediaTypeId());
+            Assert.assertEquals("Title" + AvMediaMockDataFactory.TEST_PROJECT_ID, dto.getTitle());
+            Assert.assertEquals(2018, dto.getYear());
+            Assert.assertEquals("/FilePath/" + AvMediaMockDataFactory.TEST_PROJECT_ID, dto.getArtWorkPath());
+            Assert.assertEquals("ProjectFileName" + AvMediaMockDataFactory.TEST_PROJECT_ID, dto.getArtWorkFilename());
+            Assert.assertEquals(12.99, dto.getCost(), 0);
+            Assert.assertEquals("ProjectCommentsFor" + AvMediaMockDataFactory.TEST_PROJECT_ID, dto.getComments());
+            Assert.assertEquals(1, dto.getRippedInd());
+            Assert.assertEquals(AvMediaMockDataFactory.TEST_PROJECT_ID + 1000, dto.getMasterDupId());
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            Assert.fail("An exception occurred setting properties for AV Project Adapater");
         }
     }
 }
