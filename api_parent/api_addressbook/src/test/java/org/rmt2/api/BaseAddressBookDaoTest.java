74c34822f6492f44199c9350c93631823037d48e
appdev <royterrell2@gmail.com>
2020-04-01 19:20:56 -0500

    IS-41:  Made provisions for test cases to capture "dbmsVendor" value as a result of chaning the application suite framework project to manage "DBMSVendor" property via AppServerConfig file.

--- api_parent/api_addressbook/src/test/java/org/rmt2/api/BaseAddressBookDaoTest.java
+++ api_parent/api_addressbook/src/test/java/org/rmt2/api/BaseAddressBookDaoTest.java
@@ -1,14 +1,19 @@
 package org.rmt2.api;
 
 import static org.mockito.Matchers.any;
+import static org.mockito.Matchers.eq;
 import static org.mockito.Mockito.when;
 
 import org.junit.After;
+import org.junit.Assert;
 import org.junit.Before;
 import org.mockito.Mockito;
 import org.modules.AddressBookConfigurator;
 import org.powermock.api.mockito.PowerMockito;
+import org.powermock.core.classloader.annotations.PrepareForTest;
 
+import com.api.config.AppPropertyPool;
+import com.api.config.ConfigConstants;
 import com.api.persistence.PersistenceClient;
 import com.api.persistence.db.orm.Rmt2OrmClientFactory;
 
@@ -19,6 +24,7 @@ import com.api.persistence.db.orm.Rmt2OrmClientFactory;
  *
  */
 
+@PrepareForTest({ Rmt2OrmClientFactory.class, AppPropertyPool.class })
 public class BaseAddressBookDaoTest {
     protected PersistenceClient mockPersistenceClient;
 
@@ -39,6 +45,14 @@ public class BaseAddressBookDaoTest {
         AddressBookConfigurator configurator = new AddressBookConfigurator();
         configurator.start();
         
+        PowerMockito.mockStatic(AppPropertyPool.class);
+        try {
+            when(AppPropertyPool.getProperty(eq("dbmsVendor"))).thenReturn(ConfigConstants.DBMSTYPE_ASA);
+        } catch (Exception e) {
+            e.printStackTrace();
+            Assert.fail("Mocking dbmsVendor value fetch setup failed");
+        }
+
         PowerMockito.mockStatic(Rmt2OrmClientFactory.class);
         this.mockPersistenceClient = Mockito.mock(PersistenceClient.class);
         when(Rmt2OrmClientFactory.createOrmClientInstance(any(String.class))).thenReturn(this.mockPersistenceClient);
