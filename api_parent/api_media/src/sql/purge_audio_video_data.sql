83c13e6f9631cad334af165c035a122d138b0099
appdev <royterrell2@gmail.com>
2020-08-24 03:06:37 -0500

    IS-51:  Added more calls for resetting the autoincrement seeding for artist, project, and track tables.

--- api_parent/api_media/src/sql/purge_audio_video_data.sql
+++ api_parent/api_media/src/sql/purge_audio_video_data.sql
@@ -2,11 +2,21 @@
 delete FROM AV_TRACKS;
 delete from AV_PROJECT;
 delete from AV_ARTIST;
+
+CALL sa_reset_identity('AV_ARTIST', 'DBA', 0);
+CALL sa_reset_identity('AV_PROJECT', 'DBA', 0);
+CALL sa_reset_identity('AV_TRACKS', 'DBA', 0);
+
 commit;
 */
 
-select name, count(*) from AV_ARTIST group by name having count(*) > 1 
-union
+/**
+select name 'artists', count(*) from av_artist group by name having count(*) > 1 
+select title 'projects', count(*) from AV_PROJECT group by title having count(*) > 1 
+select track_title 'tracks', count(*) from AV_TRACKS group by track_title having count(*) > 1 
+*/
+
+
 select 'artists', count(*) from av_artist
 union
 select 'projects', count(*) from AV_PROJECT
